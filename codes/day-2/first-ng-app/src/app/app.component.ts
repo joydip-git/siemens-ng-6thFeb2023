bf6bb375e62d68f06ba14ac856e5acef3026f68d
Joydip <joydip.mondal@gmail.com>
2023-02-07 14:22:07 +0530

    first angular app

---
+++ codes/day-2/first-ng-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-main',
+    template: `
+    <h2>
+        Welcome to Angular
+    </h2>
+    `
+})
+export class AppComponent {
+
+}
---
+++ codes/day-2/first-ng-app/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    declarations: [AppComponent],
+    providers: [],
+    imports: [],
+
+})
+export class AppModule {
+
+}
